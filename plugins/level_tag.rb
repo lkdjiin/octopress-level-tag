03ccca95dcc7d55914d41c1b3d670548a80ba909
lkdjiin <xavier.nayrac@gmail.com>
2013-07-30 09:30:48 +0200

    Refactoring

--- plugins/level_tag.rb
+++ plugins/level_tag.rb
@@ -1,6 +1,6 @@
 # Level tag plugin for Octopress.
 #
-# Description: Add a level label to a post.
+# Description: Add a level description to a post.
 #
 # Syntax: {% level 1|2|3 %}
 #
@@ -25,17 +25,54 @@ module Jekyll
     end
 
     def render(context)
-      config = context.registers[:site].config
-      label = config['level_tag_level'] || "Level: "
-      level = case @level
+      cfg = get_config(context)
+      html_output(get_head(cfg), get_level(cfg))
+    end
+
+    private
+
+    # Get an interface to config file (_config.yml).
+    #
+    # context - ? TODO review doc.
+    #
+    # Returns a Hash config.
+    def get_config(context)
+      context.registers[:site].config
+    end
+
+    # Get the head label (ie "Level: ").
+    #
+    # config - Hash configuration object.
+    #
+    # Returns String.
+    def get_head(config)
+      config['level_tag_level'] || "Level: "
+    end
+
+    # Get the «level description» label (ie "easy" or "hard").
+    #
+    # config - Hash configuration object.
+    #
+    # Returns String.
+    def get_level(config)
+      case @level
       when "1" then config['level_tag_level_1'] || "easy"
       when "2" then config['level_tag_level_2'] || "medium"
       when "3" then config['level_tag_level_3'] || "hard"
       else
         "unknown"
       end
+    end
+
+    # Produce html output for given head and level description.
+    #
+    # head  - String heading.
+    # level - String level description.
+    #
+    # Returns String html code to insert in the post.
+    def html_output(head, level)
       classes = "class='level-tag level-tag-#{@level}'"
-      "<div #{classes}>#{label}<span>#{level}</span></div>"
+      "<div #{classes}>#{head}<span>#{level}</span></div>"
     end
 
   end
